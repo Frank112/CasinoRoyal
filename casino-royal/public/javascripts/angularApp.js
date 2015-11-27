72ef454215cdb8bf59f24cdb53a313050a52a1bd
Frank Loeffler <FrankLoeffler404@gmail.com>
2015-11-27 18:53:43 +0100

    Its a WRAP !!!!

--- casino-royal/public/javascripts/angularApp.js
+++ casino-royal/public/javascripts/angularApp.js
@@ -1,21 +1,78 @@
 (function () {
     var app = angular.module('casino', []);
 
-    app.controller('StockCtrl', function($scope){
-        $scope.currentStockValue = 1.0;
-
-        $scope.setCurrentStockValue = function(value){
-            $scope.currentStockValue = value;
+    app.filter('moneyRound', function(){
+        return function(input){
+            return input - (input % 10);
         };
-
     });
 
-    app.controller('DrinksCtrl', function ($scope, $http) {
+    app.controller('Controller', function($scope, $http){
+        $scope.currentStockValue = 1.0;
         $scope.drinks = [];
+        $scope.moneyInfo = {};
+        $scope.lastTransaction = {value : 0};
+        $scope.statistic = [];
+        $scope.directTransactionValue = 0;
+
+        $scope.updateStatistics = function(){
+          $http.get('/statistic').success(function(data){
+            $scope.statistic = data;
+          });
+        };
+
+        $scope.getStatisticValue = function(drink){
+            var stat = {value: 0, penalty: 0};
+            for(i = 0; i < $scope.statistic.length; i++){
+                if($scope.statistic[i].drink[0] === drink._id){
+                    stat.value = $scope.statistic[i].value;
+                    stat.penalty = $scope.statistic[i].penalty;
+                    return stat;
+                }
+            }
+            return stat;
+        };
+
+        $scope.makeTransaction = function(drink, isPenalty){
+            $http.post('/transaction', {drink: drink, isPenalty: isPenalty}).success(function(data){
+                $scope.lastTransaction = data;
+                $scope.updateMoneyInfo();
+            });
+        };
+
+        $scope.makeDirectTransaction = function(isPenalty){
+            $http.post('/directTransaction', {isPenalty: isPenalty, value: $scope.directTransactionValue}).success(function(data){
+                $scope.lastTransaction = data;
+                $scope.updateMoneyInfo();
+                $scope.directTransactionValue = 0;
+            });
+        };
+
+        $http.get('/moneyInfo').success(function(data){
+            $scope.moneyInfo = data;
+        });
+
+        $scope.updateMoneyInfo = function(){
+            $http.get('/moneyInfo').success(function(data){
+                $scope.moneyInfo = data;
+            });
+        };
+
+        $scope.updateDrinks = function(){
+            $http.get('/drinks').success(function(data){
+                $scope.drinks = data;
+            });
+        };
 
         $http.get('/drinks').success(function(data){
             $scope.drinks = data;
         });
+
+        $scope.setCurrentStockValue = function(value){
+            $scope.currentStockValue = value;
+            $scope.updateDrinks();
+        };
+
     });
 
 })();
--- casino-royal/routes/index.js
+++ casino-royal/routes/index.js
@@ -17,17 +17,44 @@ var Transactions = mongoose.model('Transactions');
 
 var moneyBank = 30000;
 var moneyCirculation = 0;
-
+var drinkStatistic = [];
 var moneyDiff = function(){
   Transactions.find(function(err, tas){
     moneyDiff = 0;
     for(i = 0; i < tas.length; i++){
       moneyDiff += tas[i].value;
+      if(tas[i].drink !== []) {
+        var inserted = false;
+        for (j = 0; j < drinkStatistic.length; j++) {
+          if(drinkStatistic[j].drink[0].equals(tas[i].drink[0])){
+            if(tas[i].value < 0){
+              drinkStatistic[j].value +=1;
+            }
+            else {
+              drinkStatistic[j].penalty += 1;
+            }
+            inserted = true;
+          }
+        }
+        if(!inserted){
+          var value = 0;
+          var penalty = 1;
+          if(tas[i].value < 0){
+            value = 1;
+            penalty = 0;
+          }
+          drinkStatistic.push({drink: tas[i].drink, value: value, penalty: penalty});
+        }
+        }
     }
     moneyBank += moneyDiff;
     moneyCirculation -= moneyDiff;
   });
 }();
+
+var computeStockValue = function(){
+  return 1 - moneyCirculation / (moneyBank + moneyCirculation);
+};
 
 
 router.get('/drinks', function(req, res, next) {
@@ -49,7 +76,7 @@ router.post('/drinks', function(req, res, next) {
 });
 
 router.get('/currentStockValue', function(req, res, next){
-  res.json({value: 1 - moneyCirculation / (moneyBank + moneyCirculation)});
+  res.json({value: computeStockValue()});
 });
 
 router.get('/moneyInfo', function(req, res, next){
@@ -60,13 +87,72 @@ router.get('/moneyInfo', function(req, res, next){
 });
 
 router.post('/transaction', function(req, res, next){
-  var transaction = new Transactions(req.body);
+  var transaction = new Transactions({
+    value: function(){
+      var value = 0;
+      if(req.body.isPenalty) {
+        value = req.body.drink.penalty * (1 + 0.5 * computeStockValue());
+      }
+      else {
+        value = -req.body.drink.value * computeStockValue();
+      }
+
+      return value - (value % 10)
+    }(),
+    drink: req.body.drink
+  });
+
+    var inserted = false;
+    for (j = 0; j < drinkStatistic.length; j++) {
+      if(drinkStatistic[j].drink[0].equals(transaction.drink[0])){
+        if(transaction.value < 0){
+          drinkStatistic[j].value +=1;
+        }
+        else {
+          drinkStatistic[j].penalty += 1;
+        }
+        inserted = true;
+      }
+    }
+    if(!inserted){
+      var value = 0;
+      var penalty = 1;
+      if(transaction.value < 0){
+        value = 1;
+        penalty = 0;
+      }
+      drinkStatistic.push({drink: transaction.drink, value: value, penalty: penalty});
+    }
+
   moneyDiff += transaction.value;
+  moneyBank += transaction.value;
+  moneyCirculation -= transaction.value;
   transaction.save(function(err, transaction){
     if(err){return next(err);}
 
     res.json(transaction);
   });
+});
+
+router.post('/directTransaction', function(req, res, next){
+  var transaction = new Transactions({
+    value : function(){
+      if(req.body.isPenalty){
+        return req.body.value;}
+      return -req.body.value;
+    }()
+  });
+  moneyDiff += transaction.value;
+  moneyBank += transaction.value;
+  moneyCirculation -= transaction.value;
+  transaction.save(function(err, transaction){
+    if(err) {return next(err);}
+    res.json(transaction);
+  });
+});
+
+router.get('/statistic', function(req, res, next){
+  res.json(drinkStatistic);
 });
 
 module.exports = router;
