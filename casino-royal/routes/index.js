7ad4e0113d66d07250088eedf8cc25b132ddf589
Frank Loeffler <FrankLoeffler404@gmail.com>
2015-11-27 01:01:13 +0100

    more backend

--- casino-royal/routes/index.js
+++ casino-royal/routes/index.js
@@ -11,6 +11,10 @@ router.get('/', function(req, res, next) {
 var Drinks = mongoose.model('Drinks');
 var Transactions = mongoose.model('Transactions');
 
+var moneyContingent = 30000;
+var moneyBank = moneyContingent;
+var moneyCirculation = moneyContingent - moneyBank;
+
 router.get('/drinks', function(req, res, next) {
   Drinks.find(function(err, drinks){
     if(err){ return next(err); }
@@ -22,15 +26,32 @@ router.get('/drinks', function(req, res, next) {
 router.post('/drinks', function(req, res, next) {
   var drink = new Drinks(req.body);
 
-  drink.save(function(err, drinks){
+  drink.save(function(err, drink){
     if(err){ return next(err); }
 
     res.json(drink);
   });
 });
 
-router.get('/reset', function(req, res, next){
-  Drinks.remove()
+router.get('/currentStockValue', function(req, res, next){
+  res.json({value: 1 - moneyCirculation / (moneyBank + moneyCirculation)});
+});
+
+router.get('/moneyInfo', function(req, res, next){
+  res.json({
+    bank: moneyBank,
+    circulation: moneyCirculation
+  });
+});
+
+router.post('/transaction', function(req, res, next){
+  var transaction = new Transactions(req.body);
+
+  transaction.save(function(err, transaction){
+    if(err){return next(err);}
+
+    res.json(transaction);
+  });
 });
 
 module.exports = router;
